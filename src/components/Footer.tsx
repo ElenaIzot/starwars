4414db6fab8c1fdb08b0e3cf071d9c0c405a3abc
Elena Izotova <elm57650@gmail.com>
2021-08-16 00:44:34 +0700

    add link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer(): JSX.Element {
     return (<footer className="footer container-xxl">
         <div className="footer__item">
                <p className="footer__text">&#169; 2021</p> 
-               <a className="footer__text footer__link">https://github.com/ElenaIzot/starwars</a>
+               <a className="footer__text footer__link" href="https://github.com/ElenaIzot/starwars">https://github.com/ElenaIzot/starwars</a>
         </div>
     </footer >
     )
